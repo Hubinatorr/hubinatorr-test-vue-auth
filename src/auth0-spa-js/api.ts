14504aff9b0ac310fff66e4200c5ff8b4549f3ec
Róbert Hubinák <robert.hubinak@nalgoo.com>
2023-11-28 18:09:11 +0100

    Test with files

---
+++ src/auth0-spa-js/api.ts
@@ -0,0 +1,39 @@
+import { TokenEndpointOptions, TokenEndpointResponse } from './global';
+import { DEFAULT_AUTH0_CLIENT } from './constants';
+import { getJSON } from './http';
+import { createQueryParams } from './utils';
+
+export async function oauthToken(
+  {
+    baseUrl,
+    timeout,
+    audience,
+    scope,
+    auth0Client,
+    useFormData,
+    ...options
+  }: TokenEndpointOptions,
+  worker?: Worker
+) {
+  const body = useFormData
+    ? createQueryParams(options)
+    : JSON.stringify(options);
+
+  return await getJSON<TokenEndpointResponse>(
+    `${baseUrl}/auth/oauth2/token`,
+    timeout,
+    audience || 'default',
+    scope,
+    {
+      method: 'POST',
+      body,
+      headers: {
+        'Content-Type': useFormData
+          ? 'application/x-www-form-urlencoded'
+          : 'application/json'
+      }
+    },
+    worker,
+    useFormData
+  );
+}
